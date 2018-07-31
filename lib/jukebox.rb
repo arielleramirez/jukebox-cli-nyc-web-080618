bf59cf8c2fdb66eb9a9422669be3d244abf5f0a1
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-31 08:45:36 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,3 +42,27 @@ def play(songs)
   end
 end
 
+def run(songs)
+  # help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(songs)
+    when 'play'
+      list(songs)
+      play(songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
+
